2af18cbee4f69063219ba9e915a77a2ff2287acd
parkjaehan <mitshowme@naver.com>
2025-06-19 22:27:30 +0900

    .

--- quartz/components/Disqus.tsx
+++ quartz/components/Disqus.tsx
@@ -1,26 +1,16 @@
-import { QuartzComponentConstructor } from "./types"
+import { FunctionalComponent, h } from "preact"
+import { useEffect } from "preact/hooks"
 
-export default (() => {
-  return () => (
-    <>
-      <div id="disqus_thread" style={{ marginTop: "4rem" }}></div>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            var disqus_config = function () {
-              this.page.url = window.location.href;
-              this.page.identifier = window.location.pathname;
-            };
+const Disqus: FunctionalComponent = () => {
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.src = "https://parkjaehan.disqus.com/embed.js"
+    script.setAttribute("data-timestamp", `${+new Date()}`)
+    script.async = true
+    document.body.appendChild(script)
+  }, [])
 
-            (function() {
-              var d = document, s = d.createElement('script');
-              s.src = 'https://parkjaehan.disqus.com/embed.js';
-              s.setAttribute('data-timestamp', +new Date());
-              (d.head || d.body).appendChild(s);
-            })();
-          `,
-        }}
-      />
-    </>
-  )
-}) satisfies QuartzComponentConstructor
+  return <div id="disqus_thread" style={{ marginTop: "4rem" }} />
+}
+
+export default Disqus
